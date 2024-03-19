4dd353635a1ce1f949ef444bb4fa89d61e4654a2
Khushi21Sharma <140005227+Khushi21Sharma@users.noreply.github.com>
2024-03-19 23:41:01 +0530

    auth page added

---
+++ src/Auth/index.tsx
@@ -0,0 +1,37 @@
+import Navbar from '@/Components/Navbar';
+import AuthModal from '@/Modals/AuthModal';
+import { authModalState } from '@/atoms/authModalAtom';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useRecoilValue } from 'recoil';
+import { auth } from "@/Firebase/firebase";
+
+type authProps = {
+};
+
+const authPage:React.FC<authProps> = () => {
+    const authModal = useRecoilValue(authModalState);
+	const [user, loading, error] = useAuthState(auth);
+	const [pageLoading, setPageLoading] = useState(true);
+	const router = useRouter();
+
+	useEffect(() => {
+		if (user) router.push("/");
+		if (!loading && !user) setPageLoading(false);
+	}, [user, router, loading]);
+
+    if (pageLoading) return null;
+    return (
+    <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
+        <div className='max-w-7xl mx-auto'>
+            <Navbar />
+            <div className='flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none'>
+					<img src='/hero.png' alt='Hero img' width={700} height={700} />
+				</div>
+                {authModal.isOpen && <AuthModal/>}
+        </div>
+    </div>
+    );
+}
+export default authPage;
